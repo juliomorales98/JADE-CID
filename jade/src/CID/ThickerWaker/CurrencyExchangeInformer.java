9b3154d1c1dec12905da1f7a2a7a243b031083a1
juliomorales98 <juliocesar.mr@protonmail.com>
2020-03-23 15:36:26 -0600

    Se imprime bonita la información de la api

--- jade/src/CID/ThickerWaker/CurrencyExchangeInformer.java
+++ jade/src/CID/ThickerWaker/CurrencyExchangeInformer.java
@@ -12,8 +12,8 @@ public class CurrencyExchangeInformer{
     private String urlApi = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=";
     private static HttpURLConnection connection;
 
-    public void GetCE(){
-        //Method java.net.HttpURLConnection
+    public void GetCE(int counter){
+       
         try {
             int status;
             BufferedReader reader;
@@ -28,31 +28,58 @@ public class CurrencyExchangeInformer{
             connection.setReadTimeout(5000);
 
             status = connection.getResponseCode();
+
+            System.out.println("\n" + counter);
+
+            //Tiene que dar un valor en el rango de 200
             if(status > 299){
+                //Imprimimos el error
                 reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
                 while((line = reader.readLine()) != null){
                     responseContent.append(line);
                 }
-
+                
+                System.out.println("Código: " + status);
+                System.out.println(responseContent.toString());
                 reader.close();
                 
             }else{
+                //Imprimimos resultado de api
+
+                //Obtenemos resultado
                 reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 while((line = reader.readLine()) != null){
                     
                     responseContent.append(line);
                 }
 
+                
+                //Quitamos caracteres { } , "
+                //Vamos imprimiendo línea por línea
+
+                String auxLine = responseContent.toString();
+                String aux = "";
+
+                for(int i = 0; i < auxLine.length(); i++){
+
+                    if(auxLine.charAt(i) != '{' && auxLine.charAt(i) != '}' && auxLine.charAt(i) != 34){
+
+                        if(auxLine.charAt(i) == ','){
+                            System.out.println(aux);
+                            aux = "";
+
+                        }else{
+                            aux += auxLine.charAt(i);
+                        }
+                    }
+                }
                 reader.close();
             }
 
-            System.out.println("\n");
-            System.out.println(responseContent.toString());
-
         } catch (MalformedURLException e) {
-            
+            System.out.println(e);
         } catch (IOException e){
-
+            System.out.println(e);
         }
         
     }
--- jade/src/CID/ThickerWaker/HandsOn5Agent.java
+++ jade/src/CID/ThickerWaker/HandsOn5Agent.java
@@ -6,27 +6,31 @@ import jade.core.behaviours.*;
 public class HandsOn5Agent extends Agent{
 
     private int tickerCount;
-    CurrencyExchangeInformer cei;
-    protected void setup(){
-        //tickerCount = 0;
-        
+    private CurrencyExchangeInformer cei;
 
-       //Añadimos waker
+    protected void setup(){
+
+        tickerCount = 0;
+        
         addBehaviour(new WakerBehaviour(this,3000){
 
             protected void handleElapsedTimeout(){
-                //Cuando waker finalize, imprime mensaje y añade el ticker behaviour al agente
+                //Cuando waker finalize, imprime mensaje, crea objeto para los request,
+                // hace un request con este y agrega el tickerbehaviour
+
+
                 System.out.println("Welcome! This agent conects to an API that provides realtime exchange rate from Bitcoin to USD.");
                 
                 cei = new CurrencyExchangeInformer();
-                cei.GetCE();
+                cei.GetCE(tickerCount);
+
                 addBehaviour(new TickerBehaviour(myAgent,15000){
 
                     protected void onTick(){
 
-                                                
-                        cei.GetCE();
-                        //tickerCount++;
+                        tickerCount++;                       
+                        cei.GetCE(tickerCount);
+                        
 
                         /*if(tickerCount == 1){
                             System.out.println("Gobai");
