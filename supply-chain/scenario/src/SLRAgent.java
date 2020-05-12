1ddddd502161726b3d758204385d4e40ccbc1cc1
juliomorales98 <juliocesar.mr@protonmail.com>
2020-05-12 18:00:24 -0500

    Agregamos que SLR Agent se registre en el directory facilitator

--- supply-chain/scenario/src/SLRAgent.java
+++ supply-chain/scenario/src/SLRAgent.java
@@ -1,5 +1,10 @@
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+//imports para directory facilitator
+import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.*;
+import jade.domain.FIPAException;
 
 public class SLRAgent extends Agent{
     private SLR slr;
@@ -7,7 +12,27 @@ public class SLRAgent extends Agent{
     double[] x = new double[]{23,26,30,34,43,48,52,57,58};//month
     protected void setup(){        
         slr = new SLR();
-        addBehaviour(new MyOneShotRegression());
+        //addBehaviour(new MyOneShotRegression());
+        // DescripciÃ³n del agente
+        DFAgentDescription descripcion = new DFAgentDescription();
+        descripcion.setName(getAID());
+        descripcion.addLanguages("Castellano");
+ 
+        // Descripcion de un servicio que proporciona el Agente
+        ServiceDescription servicio = new ServiceDescription();
+        servicio.setType("Data Analytics");
+        servicio.setName("Linear Regression");
+ 
+        // AÃ±ade dicho servicio a la lista de servicios de la descripciÃ³n del agente
+        descripcion.addServices(servicio);       
+ 
+        try {
+            //Registrando el Agente con sus respectivo servicios
+            DFService.register(this, descripcion);
+        }
+        catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 
     private class MyOneShotRegression extends OneShotBehaviour{
